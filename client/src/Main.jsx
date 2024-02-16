61e81aa53ee94c172ed267aaefe387aa8b6f2bc7
Firefox <tshankgg@gmail.com>
2024-02-15 21:49:27 -0600

    added some boilerplate code

---
+++ client/src/Main.jsx
@@ -0,0 +1,40 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom/dist'
+import './index.css'
+
+import App from './App.jsx'
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Error from './pages/Error.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    error: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        path: '/me',
+        element: <Profile />
+      }, {
+        path: '/profiles/:profileId',
+        element: <Profile />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
