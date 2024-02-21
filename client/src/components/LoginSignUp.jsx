d5bb1e788d1e36cd5f100a03475e89423de14ad7
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-20 21:39:38 -0600

    added the components for a login/signup portal

---
+++ client/src/components/LoginSignUp.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from'react';
+import './assets/css/LoginSignUp.css';
+
+import user_icon from './assets/img/account-box-line.png';
+import email_icon from './assets/img/at-line.png';
+import password_icon from './assets/img/lock-password-line.png';
+
+const LoginSignUp = () => {
+
+    const [action,setAction] = useState('Sign Up');
+
+    return (
+        <div className='container'>
+            <div className='header'>
+                <div className='text'>{action}</div>
+                {/* might have to put plain text "Login" into the underline div */}
+                <div className='underline'></div>
+            </div>
+            <div className='inputs'>
+                {action==='Login'?<div></div>:<div className='input'>
+                    <img src={user_icon} alt='user' />
+                    <input type='text' placeholder='Name' />
+                </div>}
+
+                <div className='input'>
+                    <img src={email_icon} alt='user' />
+                    <input type='email' placeholder='Username' />
+                </div>
+                <div className='input'>
+                    <img src={password_icon} alt='user' />
+                    <input type='password' placeholder='Password' />
+                </div>
+                {action==='Sign Up'?<div></div>:<div className="forgot-password">Lost Password? <span>Click Here!</span></div>}                
+                <div className="submit-container">
+                    <div className={action==='Login'?'submit gray':'submit'} onClick={()=>{setAction('Sign Up')}}>Sign Up</div>
+                    <div className={action==='Sign Up'?'submit gray':'submit'} onClick={()=>{setAction('Login')}}>Login</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginSignUp;
---
+++ client/src/pages/login.js
@@ -0,0 +1,11 @@
+import LoginSignUp from '../components/LoginSignUp';
+
+function App() {
+  return (
+    <div>
+      <LoginSignUp />
+    </div>
+  );
+}
+
+export default App;
