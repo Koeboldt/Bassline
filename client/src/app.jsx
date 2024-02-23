ab0d145decb7e73bb81744dfacddfb9d63817bf2
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2024-02-22 19:31:27 -0600

    fresh vite app with antd.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,48 +1,18 @@
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    createHttpLink,
-  } from '@apollo/client';
-  import { setContext } from '@apollo/client/link/context';
-  import { Outlet } from 'react-router-dom';
-  
-  import Header from './components/Header';
-  import Footer from './components/Footer';
-  
-  const httpLink = createHttpLink({
-    uri: '/graphql',
-  });
-  
-  const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists
-    const token = localStorage.getItem('id_token');
-    // return the headers to the context so httpLink can read them
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    };
-  });
-  
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
-  
-  function App() {
-    return (
-      <ApolloProvider client={client}>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <div className="container">
-            <Outlet />
-          </div>
-          <Footer />
-        </div>
-      </ApolloProvider>
-    );
-  }
-  
-  export default App;
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ErrorPage from './pages/Error'
+import Login from './pages/Login1'
+
+
+function App() {
+
+  return (
+    <>
+      <Login></Login>
+    </>
+  )
+}
+
+export default App
