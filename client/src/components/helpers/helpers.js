9e6a9dd321b5550f8743da6373098180d03d4d8a
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-22 19:41:06 -0600

    created a helper for the signup for to verify with the regex

---
+++ client/src/components/helpers/helpers.js
@@ -0,0 +1,12 @@
+export function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+export function validatePassword(password) {
+    const re = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    if (password.match(re)) {
+        return true;
+    }
+    return false;
+}
