8578dee5d602aac7faa94d605e610af54b9d43f8
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2024-02-22 20:08:03 -0600

    added the profile page and changed some things to make the client run.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ErrorPage from './pages/Error'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import Login from './pages/Login1'
-
 
 function App() {
 
