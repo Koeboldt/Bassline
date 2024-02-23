05e290b7d0d1ca712f302b5fe012e80a94afbdf4
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2024-02-22 19:43:18 -0600

    fixed some things.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ErrorPage from './pages/Error'
-import Login from './pages/Login1'
 
+import Login from './pages/Login1'
 
 function App() {
 
