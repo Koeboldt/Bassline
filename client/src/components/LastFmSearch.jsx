3e510b8966df1c3a974357230dc2809b498101c5
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2024-02-26 13:01:02 -0600

    Created an outline of what will determine what the type of the search is based on the user input.

--- client/src/components/LastFmSearch.jsx
+++ client/src/components/LastFmSearch.jsx
@@ -1,4 +1,22 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const LastFmSearch = () => {}
+const LastFmSearch = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState('[]');
+    const [searchType, setSearchType] = useState('');
+
+const determineSearchType = () => {
+    if (lowerCaseTerm.includes('artist')){
+        setSearchType('artist');
+    }else if (lowerCaseTerm.includes('album')){
+        setSearchType('album');
+    }else if (lowerCaseTerm.includes('track') || lowerCaseTerm.includes('song')){
+        setSearchType('track');
+    }else{
+        //default to artist if no search type is found.
+        setSearchType('artist');
+    }
+};
+
+};
