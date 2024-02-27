997421a06deae04df0274976618e787d38c870c6
Firefox <tshankgg@gmail.com>
2024-02-26 20:12:59 -0600

    navbar functionality, created a home and search page

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,13 +2,13 @@ import { Outlet } from 'react-router-dom'
 import Login from './pages/Login1'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import 'antd/dist/antd.css';
 
 function App() {
 
   return (
     <>
     <Header />
-      <Login />
       <Outlet />
       <Footer />
     </>
