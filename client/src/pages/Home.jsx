794170d214eb33276c4cba5d8e13222562adc848
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-27 19:34:03 -0600

    dialed back the opacity on the elements

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,11 +5,11 @@ export default function Home() {
     return (
         <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', minHeight: '100vh', position: 'relative' }}>
             <Affix style={{ position: 'fixed', top: 0, left: 0, zIndex: 999 }}>
-                <div style={{ padding: '10px', borderRadius: '10px', backgroundColor: 'rgba(255, 255, 255, 0.5)', color: 'white' }}>
+                <div style={{ padding: '10px', borderRadius: '10px', backgroundColor: 'rgba(255, 255, 255, 0.15)', color: 'white' }}>
                     <h1>Bassline!</h1>
                 </div>
             </Affix>
-            <div style={{ position: 'absolute', bottom: 15, right: 15, marginRight: '20px', marginBottom: '20px', padding: '10px', borderRadius: '10px', backgroundColor: 'rgba(255, 255, 255, 0.5)' color: 'white' }}>
+            <div style={{ position: 'absolute', bottom: 15, right: 15, marginRight: '20px', marginBottom: '20px', padding: '10px', borderRadius: '10px', backgroundColor: 'rgba(255, 255, 255, 0.15)' color: 'white' }}>
                 <p>
                     Welcome to Bassline, a place for music buffs of every genre to come and discuss their favorite artists, albums, and songs! 
                     Whether you'd like to study some timeless classics, or are looking for todays hottest hits, Bassline has you covered with a deep community that shares your passion!
