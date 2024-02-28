4cd192d975fcd8e86ba4b46fd7ba46cb7757a7b8
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-27 18:50:48 -0600

    added the background image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Affix } from 'antd';
+import backgroundImage from '../components/assets/img/Head_of_Five_Strings_Bass_Guitar_51875534926.jpg'
 
 export default function Home() {
     return (
