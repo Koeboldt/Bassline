99901608855d3d0e708b5b211a02337066caa899
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-27 20:03:38 -0600

    added an img to the error page and stylized

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,4 +1,5 @@
 import {useRouteError} from "react-router-dom";
+import { Row, Col } from 'antd';
 
 export default function ErrorPage () {
     const error = useRouteError();
@@ -6,11 +7,20 @@ export default function ErrorPage () {
 
     return(
         <div id="error page">
-            <h1>Unexpected error has occurred</h1>
-            <p>Error</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
+            <Row justify="center">
+                <Col xs={24} sm={12} md={10} lg={8} xl={6}>
+                    <img src="../components/assets/img/Smashed_Gutars_Matt_Sorum_6465370891.jpg" alt="Smashed Guitar" />
+                </Col>
+                <Col xs={24} sm={12} md={14} lg={16} xl={18}>
+                    <div className="error-content">
+                        <h1>Unexpected error has occurred</h1>
+                        <p>Error</p>
+                        <p>
+                            <i>{error.statusText || error.message}</i>
+                        </p>
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 }
