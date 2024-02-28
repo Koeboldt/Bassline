21bbff78676a50344f5c2353b288abf12e53bfde
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-27 18:46:32 -0600

    adding some stylizing to the Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import { Affix } from 'antd';
+
 export default function Home() {
     return (
         <div>
-            <h1>Bassline!</h1>
+            <Affix style={{ position: 'fixed', top: 0, left: 0, zIndex: 999 }}>
+                <h1>Bassline!</h1>
+            </Affix>
             <p>
                 Welcome to Bassline, a place for music buffs of every genre to come and discuss their favorite artists, albums, and songs! 
                 Whether you'd like to study some timeless classics, or are looking for todays hottest hits, Bassline has you covered with a deep community that shares your passion!
